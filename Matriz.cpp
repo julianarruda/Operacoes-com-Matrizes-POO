2d3297eb4411956769bec9fa656f46ce8c607479
Juliana Narcizo <42411447+julianarruda@users.noreply.github.com>
2019-05-02 08:52:38 -0300

    Update Matriz.cpp

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,1 +1,16 @@
+#include "Matriz.h"
 
+Matriz::Matriz(){
+    int _numero_de_linhas = linhas;
+    linhas = 0;
+    int _numero_de_colunas = colunas;
+    colunas = 0;
+}
+Matriz::Matriz(int linhas, int colunas, const double &valor = 0){
+}
+
+Matriz::Matriz(const Matriz &m){
+}
+
+Matriz::~Matriz(){
+}
