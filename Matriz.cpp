a5bca4292819ad74bb07e35c21bfa486056e7c98
eduardaavila <50221826+eduardaavila@users.noreply.github.com>
2019-05-05 20:47:53 -0300

    Update Matriz.cpp

--- Matriz.cpp
+++ Matriz.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 Matriz::Matriz(){
-    _numero_de_linhas = 0;
-    _numero_de_colunas = 0;
+    this->_numero_de_linhas = 0;
+    this->_numero_de_colunas = 0;
 }
 
 Matriz::Matriz(unsigned int linhas, unsigned int colunas, const double &valor){
@@ -13,27 +13,50 @@ Matriz::Matriz(unsigned int linhas, unsigned int colunas, const double &valor){
         cin >> linhas;
         cin >> colunas;
     }
+
     this->_numero_de_linhas = linhas;
     this->_numero_de_colunas = colunas;
-    for(unsigned int i=0; i < _numero_de_linhas; i++){
-        for(unsigned int j=0; j < _numero_de_colunas; j++){
-            M[i][j] = valor;
-        }
-    }
+
+    this->M = new double*[linhas];
+    for (unsigned int i = 0; i < linhas; ++i)
+    this->M[i] = new double[colunas];
+
+    for(unsigned int i = 0; i < linhas; i++)
+        for(unsigned int j = 0; j < colunas; j++)
+            this->M[i][j] = valor;
 }
 
 Matriz::Matriz(const Matriz &m){
     this->_numero_de_linhas = m._numero_de_linhas;
     this->_numero_de_colunas = m._numero_de_colunas;
+
+    this->M = new double*[_numero_de_linhas];
     for (unsigned int i = 0; i < _numero_de_linhas; ++i)
-        this->M[i] = new int[_numero_de_colunas];
+        this->M[i] = new double[_numero_de_colunas];
+
     for(unsigned int i = 0; i < _numero_de_linhas; i++)
         for(unsigned int j = 0; j < _numero_de_colunas; j++)
             this->M[i][j] = m.M[i][j];
 }
 
 Matriz::~Matriz(){
-    //destrui a matriz
+   for(unsigned int i = 0; i < this->_numero_de_linhas; ++i)
+        delete [] this->M[i];
+    delete [] this->M;
+}
+
+void Matriz::ValorLinha(){
+    int linhas;
+    cout << "Insira a quantidade de linhas da matriz:" << endl;
+    cin >> linhas;
+    this->_numero_de_linhas = linhas;
+}
+
+void Matriz::ValorColuna(){
+    int colunas;
+    cout << "Insira a quantidade de colunas da matriz:" << endl;
+    cin >> colunas;
+    this->_numero_de_colunas = colunas;
 }
 
 void Matriz::MatrizIdentidade(){
@@ -47,6 +70,7 @@ void Matriz::MatrizIdentidade(){
     }
 }
 
+
 void Matriz::AlteraValor(){
     unsigned int nL, nC;
     char resp[3];
@@ -57,17 +81,241 @@ void Matriz::AlteraValor(){
     cin >> this->M[nL][nC];
 }
 
-Matriz Matriz::SomaMatriz(Matriz a){
+/*Matriz Matriz::SomaMatriz(Matriz a){
     if(this->_numero_de_linhas!=a._numero_de_linhas && this->_numero_de_colunas!=a._numero_de_colunas){
        cout << "Operacao invalida!";
        exit(-1);
     }
     else{
         Matriz aux;
-        for(unsigned int i = 0; i < _numero_de_linhas; i++)
-        for(unsigned int j = 0; j < _numero_de_colunas; j++)
+        for(unsigned int i = 0; i < a._numero_de_linhas; i++)
+        for(unsigned int j = 0; j < a._numero_de_colunas; j++)
             aux.M[i][j] = this->M[i][j] + a.M[i][j];
 
         return aux;
     }
+}
+*/
+
+Matriz& Matriz::operator+=(const Matriz &a){
+
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++)
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            this->M[i][j] += a.M[i][j];
+
+    return *this;
+}
+
+const Matriz Matriz::operator+(const Matriz &a) const{
+    if(this->_numero_de_linhas!=a._numero_de_linhas && this->_numero_de_colunas!=a._numero_de_colunas){
+       cout << "Operacao invalida!";
+       exit(-1);
+    }
+    Matriz m(*this);
+    m += a;
+    return m;
+}
+
+/*Matriz Matriz::SubtraiMatriz(Matriz a){
+    if(this->_numero_de_linhas!=a._numero_de_linhas && this->_numero_de_colunas!=a._numero_de_colunas){
+       cout << "Operacao invalida!";
+       exit(-1);
+    }
+    else{
+        Matriz aux;
+        for(unsigned int i = 0; i < a._numero_de_linhas; i++)
+        for(unsigned int j = 0; j < a._numero_de_colunas; j++)
+            aux.M[i][j] = this->M[i][j] - a.M[i][j];
+
+        return aux;
+    }
+}
+*/
+
+Matriz& Matriz::operator-=(const Matriz &a){
+
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++)
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            this->M[i][j] -= a.M[i][j];
+
+    return *this;
+}
+
+const Matriz Matriz::operator-(const Matriz &a) const{
+    if(this->_numero_de_linhas!=a._numero_de_linhas && this->_numero_de_colunas!=a._numero_de_colunas){
+       cout << "Operacao invalida!";
+       exit(-1);
+    }
+    Matriz m(*this);
+    m -= a;
+    return m;
+}
+
+/*Matriz Matriz::Trasposta(){
+    Matriz aux;
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++)
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            aux.M[i][j] = this->M[j][i];
+
+    return aux;
+}
+*/
+
+const Matriz Matriz::operator~(const Matriz &a) const{
+    Matriz aux(*this);
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++)
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            aux.M[i][j] = this->M[j][i];
+            
+    return aux;
+}
+
+/*Matriz Matriz::MultiplicaMatrizes(Matriz a){
+    Matriz m;
+    if(this->_numero_de_colunas != a._numero_de_linhas){
+        cout << "Operacao invalida!";
+        exit(-1);
+    }
+    else{
+        int Aux=0;
+        m._numero_de_linhas=this->_numero_de_linhas;
+        m._numero_de_colunas=a._numero_de_colunas;
+        for(unsigned int i=0; i<this->_numero_de_linhas; i++){
+            for(unsigned int j=0; j<a._numero_de_colunas; j++){
+                 m.M[i][j]=0;
+                 for(unsigned int k=0; k<a._numero_de_linhas; k++){
+                    Aux += this->M[i][k] * a.M[k][j];
+                 }
+                 m.M[i][j]=Aux;
+                 Aux=0;
+            }
+        }
+    }
+    return m;
+}
+*/
+
+const Matriz Matriz::operator*(const Matriz &a) const {
+    if(this->_numero_de_colunas != a._numero_de_linhas){
+        cout << "Operacao invalida!";
+        exit(-1);
+    }
+    else{
+        Matriz m(*this);
+        int Aux=0;
+    
+        m._numero_de_linhas=this->_numero_de_linhas;
+        m._numero_de_colunas=a._numero_de_colunas;
+        for(unsigned int i=0; i<this->_numero_de_linhas; i++){
+            for(unsigned int j=0; j<a._numero_de_colunas; j++){
+                m.M[i][j]=0;
+                for(unsigned int k=0; k<a._numero_de_linhas; k++){
+                    Aux += this->M[i][k] * a.M[k][j];
+                }
+                m.M[i][j]=Aux;
+                Aux=0;
+            }
+        }
+        return m;
+    }
+}
+
+/*int Matriz::Igualdade(Matriz a){
+    unsigned int cont=0;
+    if(this->_numero_de_linhas!=a._numero_de_linhas || this->_numero_de_colunas!=a._numero_de_colunas){
+        cont = 0;
+    }
+    else{
+        for(unsigned int i = 0; i < this->_numero_de_linhas; i++){
+            for(unsigned int j = 0; j < this->_numero_de_colunas; j++){
+                if(this->M[i][j] == a.M[i][j]){
+                    cont += 1;
+                }
+            }
+        }
+    }
+    
+    if (cont == (a._numero_de_linhas*a._numero_de_colunas))
+        return 1;
+    else
+        return 0;
+}
+*/
+
+const int Matriz::operator==(const Matriz &a) const{
+    unsigned int cont=0;
+    if(this->_numero_de_linhas!=a._numero_de_linhas || this->_numero_de_colunas!=a._numero_de_colunas){
+        cont = 0;
+    }
+    else{
+        for(unsigned int i = 0; i < this->_numero_de_linhas; i++){
+            for(unsigned int j = 0; j < this->_numero_de_colunas; j++){
+                if(this->M[i][j] == a.M[i][j]){
+                    cont += 1;
+                }
+            }
+        }
+    }
+    
+    if (cont == (a._numero_de_linhas*a._numero_de_colunas))
+        return 1;
+    else
+        return 0;
+}
+
+/*void Matriz::ImprimeMatriz(){
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++) {
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            cout << this->M[i][j] << "\t";
+        cout << endl;
+    }
+}
+*/
+
+/*void Matriz::LeituraMatriz(){
+    cout << "Insira os valores da matriz:" << endl;
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++)
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++)
+            cin >> this->M[i][j];
+}
+*/
+
+void Matriz::ModificaElementos0(){
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++){
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++){
+            this->M[i][j] = 0;
+        }
+    }
+}
+
+void Matriz::ModificaElementos1(){
+    for(unsigned int i = 0; i < this->_numero_de_linhas; i++){
+        for(unsigned int j = 0; j < this->_numero_de_colunas; j++){
+            this->M[i][j] = 1;
+        }
+    }
+}
+
+Matriz& Matriz::operator=( const Matriz &m ){
+    for (unsigned int i = 0; i < m._numero_de_linhas; ++i)
+        memcpy( this->M[i], m.M[i], sizeof(int) * m._numero_de_colunas );
+    return *this;
+}
+
+ostream &operator<<(ostream &out, const Matriz &m ){
+    for(unsigned int i = 0; i < m._numero_de_linhas; i++) {
+        for(unsigned int j = 0; j < m._numero_de_colunas; j++)
+            out << m.M[i][j] << "\t";
+        out << endl;
+    }
+    return out;
+}
+
+istream &operator>>(istream &in, Matriz &m){
+    for(unsigned int i = 0; i < m._numero_de_linhas; i++)
+        for(unsigned int j = 0; j <m._numero_de_colunas; j++){
+            cout << "(" << i << "," << j << "): ";
+            in >> m.M[i][j];
+        }
+    return in;
 }
