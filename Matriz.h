5daa534fec583f0d464ecb7188def8cbf9ea1c2b
Juliana Narcizo <42411447+julianarruda@users.noreply.github.com>
2019-05-06 00:21:50 -0300

    Update Matriz.h

--- Matriz.h
+++ Matriz.h
@@ -1,31 +1,50 @@
 //*****************************TRABALHO PRÁTICO I - PROGRAMAÇÃO ORIENTADA A OBJETOS **********************************************//
-#ifndef _MATRIZ_H
-#define _MATRIZ_H
+#ifndef MATRIZ_H
+#define MATRIZ_H
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
 class Matriz{
 public:
     Matriz(); //cria uma matriz vazia com número de linhas e colunas iguais a 0.
-    Matriz(int linhas, int colunas, const double &valor = 0); //cria uma matriz com o número de linhas e colunas iguais aos recebidos pelos argumentos.
+    Matriz(unsigned int linhas, unsigned int colunas, const double &valor); //cria uma matriz com o número de linhas e colunas iguais aos recebidos pelos argumentos.
     Matriz(const Matriz &m); //Construtor de cópia.
     ~Matriz(); //destrutor.
-    double MatrizIdentidade();
-    double AlteraValor(); //Altera o valor de uma posição da matriz.
-    double ModificaElementosY(); //Modifica os elementos da matriz para 0.
-    double ModificaElementosX(); //Modifica os elementos da Matriz com 1.
-    double SomaMatriz();
-    double SubtraiMatriz();
-    double Trasposta();
-    double MultiplicaMatrizes();
-    double Igualdade(); //Verifica se duas Matrizes são iguais.
-    double ImprimeMatriz();
-    double LeituraMatriz();
+
+    friend ostream &operator<<(ostream &out, const Matriz& a);
+    friend istream &operator>>(istream &in, Matriz &a);
+
+    //int Igualdade(Matriz a); //Verifica se duas Matrizes são iguais.
+    //Matriz SomaMatriz(Matriz a);
+    //Matriz SubtraiMatriz(Matriz a);
+    //Matriz Trasposta();
+    //Matriz MultiplicaMatrizes(Matriz a);
+    Matriz operator*(const Matriz &a) const;
+    int operator==(const Matriz &a) const;
+    Matriz& operator=( const Matriz &m );
+    Matriz operator~() const;
+    Matriz& operator-=(const Matriz &a);
+    Matriz operator-(const Matriz &a) const;
+    Matriz operator+(const Matriz &a) const;
+    Matriz& operator+=(const Matriz &a);
+
+    void MatrizIdentidade();
+    void AlteraValor(); //Altera o valor de uma posição da matriz.
+    void ModificaElementos0(); //Modifica os elementos da matriz para 0.
+    void ModificaElementos1(); //Modifica os elementos da Matriz com 1.
+    void ValorLinha();
+    void ValorColuna();
+    int get_numero_de_linhas();
+    int get_numero_de_colunas();
+
 private:
     int _numero_de_linhas;
     int _numero_de_colunas;
+    double **M;
 };
-#endif // _MATRIZ_H
+
+#endif // MATRIZ_H
