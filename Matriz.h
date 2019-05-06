694dd88eda844a3f6825c42e9d48ce00cb4465a9
eduardaavila <50221826+eduardaavila@users.noreply.github.com>
2019-05-05 23:01:44 -0300

    Update Matriz.h

--- Matriz.h
+++ Matriz.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -13,17 +14,31 @@ public:
     Matriz(unsigned int linhas, unsigned int colunas, const double &valor); //cria uma matriz com o número de linhas e colunas iguais aos recebidos pelos argumentos.
     Matriz(const Matriz &m); //Construtor de cópia.
     ~Matriz(); //destrutor.
+
+    friend ostream &operator<<(ostream &out, const Matriz& a);
+    friend istream &operator>>(istream &in, Matriz &a);
+
+    //int Igualdade(Matriz a); //Verifica se duas Matrizes são iguais.
+    //Matriz SomaMatriz(Matriz a);
+    //Matriz SubtraiMatriz(Matriz a);
+    //Matriz Trasposta();
+    //Matriz MultiplicaMatrizes(Matriz a);
+    Matriz operator*(const Matriz &a) const;
+    int operator==(const Matriz &a) const;
+    Matriz& operator=( const Matriz &m );
+    Matriz operator~() const;
+    Matriz& operator-=(const Matriz &a);
+    Matriz operator-(const Matriz &a) const;
+    Matriz operator+(const Matriz &a) const;
+    Matriz& operator+=(const Matriz &a);
+
     void MatrizIdentidade();
     void AlteraValor(); //Altera o valor de uma posição da matriz.
-    void ModificaElementosY(); //Modifica os elementos da matriz para 0.
-    double ModificaElementosX(); //Modifica os elementos da Matriz com 1.
-    Matriz SomaMatriz(Matriz a);
-    double SubtraiMatriz(Matriz a);
-    double Trasposta();
-    double MultiplicaMatrizes(Matriz a);
-    int Igualdade(Matriz a); //Verifica se duas Matrizes são iguais.
-    void ImprimeMatriz();
-    void LeituraMatriz();
+    void ModificaElementos0(); //Modifica os elementos da matriz para 0.
+    void ModificaElementos1(); //Modifica os elementos da Matriz com 1.
+    void ValorLinha();
+    void ValorColuna();
+
 private:
     int _numero_de_linhas;
     int _numero_de_colunas;
